94cc48133ac80bd060841ba8c170e2046378e448
Ozodbek <ozodbekozodov0002@gmail.com>
2023-07-31 17:16:04 +0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { QuestionComponent } from './question/question.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'welcome',pathMatch:'full'},
-  {path:'welcome',component:WelcomeComponent},
-  {path:'question',component:QuestionComponent},
+  {path:'',redirectTo:'welcome',pathMatch:'full'}, // default path
+  {path:'welcome',component:WelcomeComponent}, // path to welcome component
+  {path:'question',component:QuestionComponent}, // path to question component
 
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
