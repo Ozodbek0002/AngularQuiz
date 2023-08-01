cf9d9e062349dc5ce9240ba04929d6a1427e8bd8
Ozodbek <ozodbekozodov0002@gmail.com>
2023-08-01 10:17:30 +0500

    .

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {
+
+export class WelcomeComponent implements OnInit {
+
+  @ViewChild('name') nameKey !: ElementRef;
+
+  public routeGame = "";
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+  startGame() {
+    if(this.nameKey.nativeElement.value === ''){
+      alert(' Please enter your name !!!');
+      this.routeGame = "Welcome";
+    }
+    localStorage.setItem('name', this.nameKey.nativeElement.value);
+    this.routeGame = "question";
+  }
 
 }
